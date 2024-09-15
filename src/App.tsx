b94a5acd9033a17b305e375c903e2ba961d3dff3
Mac <mac@Macs-MacBook-Pro.local>
2024-09-16 00:05:07 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-
-//Navigation
+// Navigation
 import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 // screens
-import Home from './screen/Home'
-import Details from './screen/Details'
+import Home from './screens/Home'
+import Details from './screens/Details'
 
 export type RootStackParamList = {
   Home: undefined;
-  Details: { productId: string };
+  Details: { product: Product};
+
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList >()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
-export default function App(): JSX.Element {
+const App = (): JSX.Element => {
   return (
-   <NavigationContainer>
-     <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen
-         name ='Home'
-         component={Home}
-         options={{
-          title: 'Trending Products'
-         }}
-        />
-        <Stack.Screen
-         name ='Details'
-         component={Details}
-         options={{
-          title: ' Products Details'
-         }}
-        />
-     </Stack.Navigator>
-   </NavigationContainer>
-  )
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+           <Stack.Screen
+            name="Home" 
+            component={Home}
+            options ={{
+              title: "Trending Products"
+            }}
+            />
+           <Stack.Screen
+            name="Details" 
+            component={Details}
+            options ={{
+              title: " Products deatils"
+            }}
+            />
+
+        </Stack.Navigator>
+    </NavigationContainer>
+ )
+    
 }
 
-const styles = StyleSheet.create({})
+export default App
+
